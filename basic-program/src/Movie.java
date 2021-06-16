f0e88cf992633ad0de7fb8ec3a1c94d37ac19c08
masterhyuns <masterhyuns@naver.com>
2021-06-16 17:22:53 +0900

    Step6. 대여료 계산 및 적립포인트 계산을 비디오 클래스로 옮겨 준다.

--- basic-program/src/Movie.java
+++ basic-program/src/Movie.java
@@ -20,4 +20,46 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
+    /**
+     * 가격 책정 메서드
+     * switch 문에서 타 객체의 속성을 사용하는것은 안좋다.
+     * @return
+     */
+    public double getCharge(int dayRented) {
+        double result = 0;
+        switch (getPriceCode()){
+            case Movie.REGULAR: // 고전작일경우
+                result += 2; // 누적대여료 +2
+                if(dayRented > 2){ // 대여일수가 3일 이상일때
+                    // 추가대여일수에 대한 누적 대여료 계산
+                    result += (dayRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE: // 신작일 경우
+                // 누적 대여로 일당 3
+                result += dayRented * 3;
+                break;
+            case Movie.CHILDRENS: // 아동작일 경우
+                result += 1.5; // 누적 대여로 +1.5
+                if(dayRented > 3){// 대여일수가 4일 이상일때
+                    // 추가대여일수에 대한 누적 대여료 계산
+                    result += (dayRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    /**
+     * 최신물 대여시 추가 적릭급 지급
+     * @return
+     */
+    public int getFrequentRenterPoints(int dayRented) {
+        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+        if((getPriceCode() == Movie.NEW_RELEASE) && dayRented > 1){
+            return 2;
+        }
+        return 1;
+    }
 }
--- basic-program/src/Rental.java
+++ basic-program/src/Rental.java
@@ -15,40 +15,11 @@ public class Rental {
         return dayRented;
     }
 
-    public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()){
-            case Movie.REGULAR: // 고전작일경우
-                result += 2; // 누적대여료 +2
-                if(getDayRented() > 2){ // 대여일수가 3일 이상일때
-                    // 추가대여일수에 대한 누적 대여료 계산
-                    result += (getDayRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE: // 신작일 경우
-                // 누적 대여로 일당 3
-                result += getDayRented() * 3;
-                break;
-            case Movie.CHILDRENS: // 아동작일 경우
-                result += 1.5; // 누적 대여로 +1.5
-                if(getDayRented() > 3){// 대여일수가 4일 이상일때
-                    // 추가대여일수에 대한 누적 대여료 계산
-                    result += (getDayRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+    double getCharge(){
+        return movie.getCharge(dayRented);
     }
 
-    /**
-     * 최신물 대여시 추가 적릭급 지급
-     * @return
-     */
-    public int getFrequentRenterPoints() {
-        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-        if((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDayRented() > 1){
-            return 2;
-        }
-        return 1;
+    int getFrequentRenterPoints(){
+        return movie.getFrequentRenterPoints(dayRented);
     }
 }
